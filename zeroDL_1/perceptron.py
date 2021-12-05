0ded3f177ca568198f7ec33f8e06d8a9174a31e1
ninjinx <responsura@gmail.com>
2021-12-05 20:18:25 +0900

    zeroDL_1 2章

---
+++ zeroDL_1/perceptron.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5]) # 重みとバイアスだけがAND と違う！
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5]) # 重みとバイアスだけがAND と違う！
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+v = XOR(0, 0)
+print(v)
+v = XOR(1, 0)
+print(v)
+v = XOR(0, 1)
+print(v)
+v = XOR(1, 1)
+print(v)
